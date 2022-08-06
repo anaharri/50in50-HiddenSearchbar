ea6e6c515ff7f65b16c8d0e642cdf44827d6c419
anaharri <anaharringtonb@gmail.com>
2022-08-05 23:55:48 -0300

    feature: random quote picker & minor changes in DOM manipulation

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const search = document.querySelector(".search");
-const btn = document.querySelector(".btn");
-const input = document.querySelector(".input");
+const search = document.querySelector(".search"),
+  btn = document.querySelector(".btn");
+const input = $(".input"),
+  quote = $("#quote"),
+  author = $("#author");
+
+function randomPicker(array) {
+  const idx = Math.floor(Math.random() * array.length);
+  return array[idx];
+}
 
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
   input.focus();
+});
+
+input.keydown((e) => {
+  const { keyCode } = e;
+
+  if (keyCode === 13) {
+    $.get(`https://quotable.io/search/quotes?query=${input.val()}`, (data) => {
+      const selectedQuote = randomPicker(data.results);
+      quote.empty();
+      quote.append(`<p>${selectedQuote.content}</p>`);
+      author.text(selectedQuote.author);
+      input.val("");
+      search.classList.toggle("active");
+    });
+
+    document.querySelector(".quote-card").classList.add("visible");
+  }
 });
